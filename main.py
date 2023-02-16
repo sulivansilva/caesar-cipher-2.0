c90ae7ce0e6576542466375e21d920e1acc95a3c
DevSulivan <117742267+sulivansilva@users.noreply.github.com>
2023-02-16 18:15:46 +0000

    Programa encriptador e decriptador de mensagens criado no curso 100 Days of Code: The Complete Python Pro Bootcamp for 2023, da Udemy. 2.0

---
+++ main.py
@@ -0,0 +1,40 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+ 
+            
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text = ""
+    if cipher_direction == "decode":
+        shift_amount *= -1
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
+    print(f"Here's the {cipher_direction}d result: {end_text}\n")
+    
+#Importing and printing the logo from art.py when the program starts.
+    
+from art import logo
+
+print(logo)
+
+#Restart the program
+
+run_again = True
+while run_again:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+#If the user enters a shift that is greater than the number of letters in the alphabet.
+
+    shift = shift % 26
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+    
+    again = input("Type 'yes' if you want to go again, oterwhise type 'no'. \n")
+    
+    if again == 'no':
+        run_again = False
+        print("Goodbye!\n")
